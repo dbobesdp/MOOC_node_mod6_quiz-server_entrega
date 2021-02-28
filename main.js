7e9c7bcba2b9b2cbb64110c0c4cdba2672d80392
Daniel Bobes de Pedro <dbobesdp@gmail.com>
2021-02-28 20:18:07 +0100

    Agregado funcionalidad solicitada

--- main.js
+++ main.js
@@ -7,40 +7,43 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Import MW supporting Method Override with express
 const methodOverride = require('method-override');
-app.use(methodOverride('_method', { methods: ["POST", "GET"] }));
+app.use(methodOverride('_method', { methods: ['POST', 'GET'] }));
 
 // ========== MODEL ==========
 
 const Sequelize = require('sequelize');
 
 const options = { logging: false, operatorsAliases: false };
-const sequelize = new Sequelize("sqlite:db.sqlite", options);
+const sequelize = new Sequelize('sqlite:db.sqlite', options);
 
-const Quiz = sequelize.define( // define Quiz model (table quizzes)
-    'quiz', {
-        question: Sequelize.STRING,
-        answer: Sequelize.STRING
-    }
+const Quiz = sequelize.define(
+  // define Quiz model (table quizzes)
+  'quiz',
+  {
+    question: Sequelize.STRING,
+    answer: Sequelize.STRING,
+  }
 );
 
-(async () => {  // IIFE - Immediatedly Invoked Function Expresión
-    try {
-        await sequelize.sync(); // Syncronize DB and seed if needed
-        const count = await Quiz.count();
-        if (count === 0) {
-            const c = await Quiz.bulkCreate([
-                {question: "Capital of Italy", answer: "Rome"},
-                {question: "Capital of France", answer: "Paris"},
-                {question: "Capital of Spain", answer: "Madrid"},
-                {question: "Capital of Portugal", answer: "Lisbon"}
-            ]);
-            console.log(`DB filled with ${c.length} quizzes.`);
-        } else {
-            console.log(`DB exists & has ${count} quizzes.`);
-        }
-    } catch (err) {
-        console.log(err);
+(async () => {
+  // IIFE - Immediatedly Invoked Function Expresión
+  try {
+    await sequelize.sync(); // Syncronize DB and seed if needed
+    const count = await Quiz.count();
+    if (count === 0) {
+      const c = await Quiz.bulkCreate([
+        { question: 'Capital of Italy', answer: 'Rome' },
+        { question: 'Capital of France', answer: 'Paris' },
+        { question: 'Capital of Spain', answer: 'Madrid' },
+        { question: 'Capital of Portugal', answer: 'Lisbon' },
+      ]);
+      console.log(`DB filled with ${c.length} quizzes.`);
+    } else {
+      console.log(`DB exists & has ${count} quizzes.`);
     }
+  } catch (err) {
+    console.log(err);
+  }
 })();
 
 // ========== VIEWs ==========
@@ -55,8 +58,8 @@ const style = `
         </style>`;
 
 // View to display all the quizzes in quizzes array
-const indexView = quizzes =>
-    `<!doctype html>
+const indexView = (quizzes) =>
+  `<!doctype html>
     <html>
     <head>
         <meta charset="utf-8">
@@ -64,25 +67,28 @@ const indexView = quizzes =>
         ${style}
     </head>
     <body>
-        <h1>Quizzes</h1>` +
-    quizzes.map(quiz =>
-        `<div>
-                <a href="/quizzes/${quiz.id}/play">${quiz.question}</a>
-                <a href="/quizzes/${quiz.id}/edit"
-                   class="button">Edit</a>
-                <a href="/quizzes/${quiz.id}?_method=DELETE"
+        <h1>Quizzes</h1><div><table>` +
+  quizzes
+    .map(
+      (quiz) =>
+        `
+                <tr><td><a href="/quizzes/${quiz.id}/play">${quiz.question}</a></td>
+                <td><a href="/quizzes/${quiz.id}/edit"
+                   class="button">Edit</a></td>
+                   <td><a href="/quizzes/${quiz.id}?_method=DELETE"
                    onClick="return confirm('Delete: ${quiz.question}')"
-                   class="button">Delete</a>
-             </div>`).join("\n") +
-    `<a href="/quizzes/new" class="button">New Quiz</a>
+                   class="button">Delete</a></td>
+                   </tr>`
+    )
+    .join('\n') +
+  `</div></table><a href="/quizzes/new" class="button">New Quiz</a>
     </body>
     </html>`;
-
 
 // View with form for trying to guess quiz
 // response - text of last trial (hidden param)
 const playView = (quiz, response) =>
-    `<!doctype html>
+  `<!doctype html>
   <html>
   <head>
       <meta charset="utf-8">
@@ -102,13 +108,12 @@ const playView = (quiz, response) =>
   </body>
   </html>`;
 
-
 // View with the result of trying to guess the quiz.
 // id - played quiz id
 // msg - result of trial
 // response - user answer for next trial
 const resultView = (id, msg, response) =>
-    `<!doctype html>
+  `<!doctype html>
   <html>
   <head>
     <meta charset="utf-8">
@@ -123,10 +128,15 @@ const resultView = (id, msg, response) =>
   </body>
   </html>`;
 
+const commonPart = (quiz) => {
+  return `<input type="text" name="question" value="${quiz.question}" placeholder="Question">
+      <input type="text" name="answer" value="${quiz.answer}" placeholder="Answer">
+      `;
+};
 
 // View to show the form to create a new quiz.
-const newView = quiz => {
-    return `<!doctype html>
+const newView = (quiz) => {
+  return `<!doctype html>
   <html>
   <head>
     <meta charset="utf-8">
@@ -143,95 +153,147 @@ const newView = quiz => {
     <a href="/quizzes" class="button">Go back</a>
   </body>
   </html>`;
-}
-
+};
 
 // View to show a form to edit a given quiz.
 const editView = (quiz) => {
-    // .... introducir código
-}
-
+  return `<!doctype html>
+  <html>
+  <head>
+    <meta charset="utf-8">
+    <title>Quiz</title>
+    ${style}
+  </head>
+  <body>
+    <h1>Edit Quiz</h1>
+    <form method="POST" action="/quizzes/${quiz.id}?_method=PUT">
+      ${commonPart(quiz)}
+      <input type="submit" class="button" value="Update">
+    </form>
+    <br>
+    <a href="/quizzes" class="button">Go back</a>
+  </body>
+  </html>`;
+};
 
 // ========== CONTROLLERs ==========
 
 // GET /, GET /quizzes
 const indexController = async (req, res, next) => {
-    try {
-        const quizzes = await Quiz.findAll()
-        res.send(indexView(quizzes))
-    } catch (err) {
-        next(err);
-    }
+  try {
+    const quizzes = await Quiz.findAll();
+    res.send(indexView(quizzes));
+  } catch (err) {
+    next(err);
+  }
 };
 
 //  GET  /quizzes/:id/play
 const playController = async (req, res, next) => {
-    const id = Number(req.params.id);
-    if (Number.isNaN(id)) return next(new Error(`"${req.params.id}" should be number.`));
+  const id = Number(req.params.id);
+  if (Number.isNaN(id))
+    return next(new Error(`"${req.params.id}" should be number.`));
 
-    const response = req.query.response || "";
+  const response = req.query.response || '';
 
-    try {
-        const quiz = await Quiz.findByPk(id);
-        if (quiz) res.send(playView(quiz, response));
-        else next(new Error(`Quiz ${id} not found.`));
-    } catch (err) {
-        next(err)
-    }
+  try {
+    const quiz = await Quiz.findByPk(id);
+    if (quiz) res.send(playView(quiz, response));
+    else next(new Error(`Quiz ${id} not found.`));
+  } catch (err) {
+    next(err);
+  }
 };
 
 //  GET  /quizzes/:id/check
 const checkController = async (req, res, next) => {
-    const response = req.query.response;
+  const response = req.query.response;
 
-    const id = Number(req.params.id);
-    if (Number.isNaN(id)) return next(new Error(`"${req.params.id}" should be number.`));
+  const id = Number(req.params.id);
+  if (Number.isNaN(id))
+    return next(new Error(`"${req.params.id}" should be number.`));
 
-    try {
-        const quiz = await Quiz.findByPk(id);
-        if (!quiz) return next(new Error(`Quiz ${id} not found.`));
-        let msg = (quiz.answer.toLowerCase().trim() === response.toLowerCase().trim())
-            ? `Yes, "${response}" is the ${quiz.question}`
-            : `No, "${response}" is not the ${quiz.question}`;
-        res.send(resultView(id, msg, response));
-    } catch (err) {
-        next(err)
-    }
+  try {
+    const quiz = await Quiz.findByPk(id);
+    if (!quiz) return next(new Error(`Quiz ${id} not found.`));
+    let msg =
+      quiz.answer.toLowerCase().trim() === response.toLowerCase().trim()
+        ? `Yes, "${response}" is the ${quiz.question}`
+        : `No, "${response}" is not the ${quiz.question}`;
+    res.send(resultView(id, msg, response));
+  } catch (err) {
+    next(err);
+  }
 };
 
 // GET /quizzes/new
 const newController = async (req, res, next) => {
-    const quiz = {question: "", answer: ""};
-    res.send(newView(quiz));
+  const quiz = { question: '', answer: '' };
+  res.send(newView(quiz));
 };
 
 // POST /quizzes
 const createController = async (req, res, next) => {
-    const {question, answer} = req.body;
+  const { question, answer } = req.body;
 
-    try {
-        await Quiz.create({question, answer});
-        res.redirect(`/quizzes`);
-    } catch (err) {
-        next(err)
-    }
+  try {
+    await Quiz.create({ question, answer });
+    res.redirect(`/quizzes`);
+  } catch (err) {
+    next(err);
+  }
 };
 
 //  GET /quizzes/:id/edit
-const editController = (req, res, next) => {
-    // .... introducir código
+const editController = async (req, res, next) => {
+  // .... introducir código
+  const id = Number(req.params.id);
+  if (Number.isNaN(id))
+    return next(new Error(`"${req.params.id}" should be number.`));
+
+  try {
+    const quiz = await Quiz.findByPk(id);
+    if (quiz) res.send(editView(quiz));
+    else next(new Error(`Quiz ${id} not found.`));
+  } catch (err) {
+    next(err);
+  }
 };
 
 //  PUT /quizzes/:id
-const updateController = (req, res, next) => {
-    // .... introducir código
+const updateController = async (req, res, next) => {
+  const id = Number(req.params.id);
+  if (Number.isNaN(id))
+    return next(new Error(`"${req.params.id}" should be number.`));
+  const { question, answer } = req.body;
+  try {
+    const quiz = await Quiz.findByPk(id);
+    if (quiz) {
+      quiz.question = question;
+      quiz.answer = answer;
+      await quiz.save();
+      res.redirect(`/quizzes`);
+    } else next(new Error(`Quiz ${id} not found.`));
+  } catch (err) {
+    next(err);
+  }
 };
 
 // DELETE /quizzes/:id
-const destroyController = (req, res, next) => {
-    // .... introducir código
-};
+const destroyController = async (req, res, next) => {
+  const id = Number(req.params.id);
+  if (Number.isNaN(id))
+    return next(new Error(`"${req.params.id}" should be number.`));
 
+  try {
+    const quiz = await Quiz.findByPk(id);
+    if (!quiz) return next(new Error(`Quiz ${id} not found.`));
+    await quiz.destroy();
+    res.redirect(`/quizzes`);
+  } catch (err) {
+    next(err);
+  }
+};
 
 // ========== ROUTES ==========
 
@@ -241,22 +303,22 @@ app.get('/quizzes/:id/check', checkController);
 app.get('/quizzes/new', newController);
 app.post('/quizzes', createController);
 
-
 // ..... crear rutas e instalar los MWs para:
 //   GET  /quizzes/:id/edit
+app.get('/quizzes/:id/edit', editController);
 //   PUT  /quizzes/:id
+app.put('/quizzes/:id', updateController);
 //   DELETE  /quizzes/:id
-
+app.delete('/quizzes/:id', destroyController);
 
 app.all('*', (req, res) =>
-    res.status(404).send("Error: resource not found or method not supported.")
+  res.status(404).send('Error: resource not found or method not supported.')
 );
-
 
 // Middleware to manage errors:
 app.use((error, req, res, next) => {
-    console.log("Error:", error.message || error);
-    res.redirect("/");
+  console.log('Error:', error.message || error);
+  res.redirect('/');
 });
 
 // Server started at port 8000
